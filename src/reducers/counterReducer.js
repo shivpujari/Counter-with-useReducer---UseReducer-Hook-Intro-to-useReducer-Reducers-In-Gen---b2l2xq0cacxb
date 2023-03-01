3732463d851ec8632be72063e47f4292bb818b9d
shiv pujari <shivanipujari03@gmail.com>
2023-03-01 09:02:06 +0530

    again

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,21 @@
 const counterReducer = (state,action) =>{
+switch(action.type){
+    case 'INCREMENT':
+    return{
+        ...state,
+        counter: state.counter + 1,
+    }
+    case "DECREMENT":
+        return{
+        ...state,
+        counter: state.counter -1,
+        }
+        default :
+        return state;
+}
+
+
+
 
 }
 
